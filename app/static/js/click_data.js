b146843ac77dab3cca544595c3bf8436ff3904ae
Debalina Chowdhury <debalinachowdhury@Debalinas-Air.attlocal.net>
2024-04-01 20:20:21 -0700

    modified routing operations

--- app/static/js/click_data.js
+++ app/static/js/click_data.js
@@ -21,3 +21,26 @@ document.getElementById('oralSurgeryForm').addEventListener('submit', function(e
     // Submit the form
     this.submit();
 });
+
+var pneumoniaForm = document.getElementById('pneumoniaForm');
+
+// Now you can perform any operations you want with the pneumoniaForm element, for example:
+if (pneumoniaForm) {
+    console.log('Pneumonia form found:', pneumoniaForm);
+    // You can access its properties, add event listeners, etc.
+} else {
+    console.error('Pneumonia form not found.');
+}
+
+document.getElementById('pneumoniaForm').addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Log to console to check if event listener is triggered
+    console.log('Pneumonia form submitted');
+
+    // Submit the form
+    this.submit();
+});
+
+
