0c72a34aa890bdf950937921264a1c83a74f51e3
davidc0126 <cjun5217@gmail.com>
2024-04-07 13:05:14 -0700

    add userstory 1 test

--- app/keys.py
+++
@@ -1,5 +0,0 @@
-account_sid = 'ACb50ea229c1a56b5f5a9134a1331d5d5f'
-auth_token = '3f4a3d2961f08b86500f40be8552ebe9'
-
-twilio_number = '+18449605383'
-target_number = '+18584316550'
--- config.py
+++ config.py
@@ -7,8 +7,4 @@ import secrets
 
 class Config:
     SECRET_KEY = secrets.token_hex(16)  # Generate a random hexadecimal string of 16 bytes (32 characters)
-    OPENAI_API_KEY = "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"
-    ACCOUNT_SID = "ACb50ea229c1a56b5f5a9134a1331d5d5f"
-    AUTH_TOKEN = "3f4a3d2961f08b86500f40be8552ebe9"
-    TWILIO_NUMBER = "+18449605383"
-    TARGET_NUMBER = "+18584316550"
+  
