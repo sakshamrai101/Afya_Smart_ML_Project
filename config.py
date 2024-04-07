2567317117102324170cb4a14a750b7fd23b6e5d
davidc0126 <cjun5217@gmail.com>
2024-04-06 23:48:37 -0700

    fix view bugs

--- config.py
+++ config.py
@@ -7,4 +7,8 @@ import secrets
 
 class Config:
     SECRET_KEY = secrets.token_hex(16)  # Generate a random hexadecimal string of 16 bytes (32 characters)
-    
+    OPENAI_API_KEY = ""
+    ACCOUNT_SID = ""
+    AUTH_TOKEN = ""
+    TWILIO_NUMBER = ""
+    TARGET_NUMBER = ""
