8de659a600c859f07ad4c32ee701d8e3f2a103c4
Debalina Chowdhury <debalinachowdhury@Debalinas-Air.attlocal.net>
2024-04-08 07:02:30 -0700

    deleted frontend folder

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import secrets
 
 class Config:
     SECRET_KEY = secrets.token_hex(16)  # Generate a random hexadecimal string of 16 bytes (32 characters)
-    OPENAI_API_KEY = ""
+    OPENAI_API_KEY = "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"
     ACCOUNT_SID = ""
     AUTH_TOKEN = ""
     TWILIO_NUMBER = ""
